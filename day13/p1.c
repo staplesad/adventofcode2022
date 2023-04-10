12d058a223e5bef66b8a96ad6195dcc82e5db769
staplesad <sadhbhsdoyle@gmail.com>
2023-04-10 22:00:16 +0200

    working non-optimal

--- day13/p1.c
+++ day13/p1.c
@@ -1,110 +1,134 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define BUF_LEN 1000
-//actually implement a tree?
+#define N_CHILD 66
 
-bool valid_pair(char first[BUF_LEN], char second[BUF_LEN]){
-  int i=0;
-  int j=0;
-  printf("first: %s\nsecond: %s\n", first, second);
-  int promote_first = 0;
-  int promote_second = 0;
-  int first_indent = 0;
-  int second_indent = 0;
-  while(i<BUF_LEN && j<BUF_LEN){
-    printf("loop i, j:\t%d, %d\nvals:\t%c, %c\n", i, j, first[i], second[j]);
-    if (first[i]==second[j]){
-      if (first[i] == '['){
-        first_indent++;
-        second_indent++;
-      } else if (first[i] == ']') {
-        first_indent--;
-        second_indent--;
-      }
-      if(promote_first==0){
-        i++;
-      }
-      if(promote_second==0){
-        j++;
-      }
-    } else {
-      if (first[i]=='\n'){
-        return true;
-      }
-      else if (second[j]=='\n'){
-        return false;
-      }
-      else if(first[i]=='['||second[j]=='['){
-        if(first[i]=='['){
-          promote_second++;
-          i++;
-        }
-        else{
-          promote_first++;
-          j++;
-        }
-      }
-      else if(first[i]==']'||second[j]==']'){
-        if(first[i]==']'){
-          if(promote_second+second_indent==first_indent && promote_second>0){
-            promote_second--;
-            i++;
-            if(promote_second==0){
-              j++;
-            }
-          }
-          else{
-            //first finishes early
-            return true;
-          }
-        }
-        else{
-          //if(promote_first+first_indent==second_indent && promote_first>0){
-          if(promote_first>0){
-            promote_first--;
-            if (promote_first==0){
-              i++;
-            }
-            j++;
-          }
-          else{
-            //second finishes early
-            return false;
-          }
-        }
-      }
-      else if(first[i]==','){
-        if (promote_second>0){
-          //second list is longer
-          return false;
-        }
-        i++;
-      }
-      else if(second[j]==','){
-        if (promote_first>0){
-          //first llist is longer
-          return true;
-        }
-        j++;
-      }
-      else{
-        if(first[i]<second[j]){
-          return true;
-        }
-        else if(first[i]>second[j]){
-          return false;
-        }
-        else{
-          printf("not sure what other cases exist?\n");
-          i++;
-          j++;
-        }
-      }
+typedef struct tree_node {
+  bool leaf;
+  union {
+    int val;
+    struct tree_node **children;
+  };
+  int n_children;
+} tree_node;
+
+typedef struct {
+  int index;
+  tree_node *node;
+  bool valid;
+} parse_object;
+
+parse_object parse_node(char * input, int cur_pos){
+  //printf("strlen: %d, pos: %d\n", strlen(input), cur_pos);
+  switch (input[cur_pos]) {
+    case '\0': {
+                parse_object res = {.valid=false};
+                return res;
+               }
+    case 'a':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9': {
+      tree_node *node = malloc(sizeof(tree_node));
+      node->leaf = true;
+      node->val = input[cur_pos] - '0';
+      parse_object res= {.node=node, .index=cur_pos+1, .valid=true};
+      return res;
     }
-    printf("first d: %d, promote: %d\nsecond d: %d, promote: %d\n", first_indent, promote_first, second_indent, promote_second);
+    case '[': {
+      tree_node **children = malloc(sizeof(tree_node*)*N_CHILD);
+      int count = 0;
+      cur_pos++;
+      while (input[cur_pos] != ']'){
+        if (input[cur_pos]==','){
+          cur_pos++;
+          continue;
+        }
+        parse_object c = parse_node(input, cur_pos);
+        if (!c.valid){
+          break;
+        }
+        children[count] = c.node;
+        count++;
+        cur_pos = c.index;
+      }
+      tree_node * node = malloc(sizeof(tree_node));
+      node->leaf=false;
+      node->children=children;
+      node->n_children=count;
+      parse_object res = {.node=node, .index=cur_pos+1, .valid=true};
+      return res;
+    }
+    default: {
+      //printf("Reached default case in parsing switch. %d: %c", cur_pos, input[cur_pos]);
+      parse_object res;
+      return res;
+    }
   }
 }
+
+int rec_comp(tree_node left, tree_node right){
+  if (left.leaf && right.leaf) {
+    if (left.val > right.val){
+      return 0;
+    }
+    if (left.val < right.val){
+      return 1;
+    }
+    return 2;
+  } else if (!left.leaf && !right.leaf){
+    int n_c = (left.n_children >= right.n_children) ? right.n_children : left.n_children;
+    int res;
+    for (int i=0;i<n_c;i++){
+      res = rec_comp(*left.children[i], *right.children[i]);
+      if (res != 2){
+        return res;
+      }
+    }
+    if (left.n_children > right.n_children){
+      return 0;
+    }
+    if (left.n_children < right.n_children){
+      return 1;
+    }
+    return 2;
+  } else {
+    tree_node ** children = malloc(sizeof(tree_node*));
+    tree_node new_node = {.leaf=false, .children=children, .n_children=1};
+    new_node.children[0] = malloc(sizeof(tree_node));
+    if (left.leaf){
+      *(new_node.children[0]) = left;
+      return rec_comp(new_node, right);
+    }
+    *(new_node.children[0]) = right;
+    return rec_comp(left, new_node);
+  }
+}
+
+bool comp_vals(char * left, char * right){
+  // parse trees
+  parse_object res;
+  res = parse_node(left, 0);
+  tree_node left_root = *res.node;
+  res = parse_node(right, 0);
+  tree_node right_root = *res.node;
+  //comp vals
+  int r = rec_comp(left_root, right_root);
+  //printf("res: %d", r);
+  return (bool)r;
+}
+
 
 int main(void){
   char first[BUF_LEN];
@@ -114,16 +138,17 @@ int main(void){
   int sum = 0;
   while(fgets(first, BUF_LEN, stdin)){
     count++;
+    printf("count: %d\n", count);
     fgets(second, BUF_LEN, stdin);
     scanf(" ", stdin);
-    if (valid_pair(first, second)){
+    if (comp_vals(first, second)){
       printf("true\n");
       sum += count;
     }
     else{
       printf("false\n");
     }
-    printf("\n");
+    //printf("\n");
 
   }
   printf("%d\n", sum);
